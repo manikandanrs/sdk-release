cd718d9eba15120a34c56902d5cc98e82609eb93
Subramanian <subramam@amazon.com>
2016-01-06 11:47:21 -0800

    Jonathan's commit

--- service-client-2/src/main/java/com/aws/java/sdk/Foo.java
+++ service-client-2/src/main/java/com/aws/java/sdk/Foo.java
@@ -3,6 +3,6 @@ package com.aws.java.sdk;
 public class Foo {
 
     public static void main() {
-        System.out.println("hello from service-client-2");
+        System.out.println("hello from service-client-2 - Jonathan ask me to change.");
     }
 }
