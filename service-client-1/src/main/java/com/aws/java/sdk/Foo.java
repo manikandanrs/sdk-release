8cb65ed5eaf96a0fffd3405040b646b971bfe94e
Subramanian <subramam@amazon.com>
2016-01-06 11:39:53 -0800

    Add new change to service-client-1

--- service-client-1/src/main/java/com/aws/java/sdk/Foo.java
+++ service-client-1/src/main/java/com/aws/java/sdk/Foo.java
@@ -3,6 +3,6 @@ package com.aws.java.sdk;
 public class Foo {
 
     public static void main() {
-        System.out.println("hello from service-client-1");
+        System.out.println("hello from service-client-1 for 1.3.1");
     }
 }
